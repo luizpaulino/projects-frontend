192c97bde302d317065316bb15c582d55f1d9dc3
316880 <luiz.oliveira@userede.com.br>
2020-04-11 04:08:39 -0300

    Implementado funcionalidade de listagem dos projetos ao entrar no app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from 'services/api';
 
 import "./styles.css";
 
 function App() {
+  const [ projects, setProjects ] = useState([]);
+
+  useEffect(() => { 
+    api.get('/projects').then( response =>{
+      setProjects(response.data)
+    });
+   }, []);
+
   async function handleAddRepository() {
     // TODO
   }
@@ -14,13 +24,16 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        { projects.map( project => (
+          <li key={project.id}>
+            
+            { project.title }
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(1)}>
+              Remover
+            </button>
+          </li> 
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
